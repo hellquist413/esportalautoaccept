bc934a8fbcdcdf2c0175a2ceb1a8f736a29a8aff
hellquist413 <hellquistmusic@gmail.com>
2022-11-13 21:33:37 +0100

    Update click.js
    fix twitch element

--- click.js
+++ click.js
@@ -5,22 +5,20 @@ chrome.storage.local.get('enabled', data => {
     if(enabled && typeof init === 'undefined') {
         console.log("Script enable waiting for match!");  
        
-                function getButtonPath (path) {
-                    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-                }
+        function getButtonPath (path) {
+            return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        }
                 
-                function update() {
-                        let acceptbtn = '//*[@id="root"]/div[3]/div[2]/div/div[3]/button[2]';
-                        if (getButtonPath(acceptbtn) == null) {
-                        } else {
-                            getButtonPath(acceptbtn).click();
-                        }
-                }
-                setInterval(function() {
-                        update();
-                }, 5000);
-    } else {
-        console.log("Script disabled returning...");
+        function update() {
+            let acceptbtn = '//*[@id="root"]/div[3]/div[2]/div/div[3]/button[2]';
+            if (getButtonPath(acceptbtn) == null) {
+            } else {
+                getButtonPath(acceptbtn).click();
+            }
+        }
+        setInterval(function() {
+            update();
+        }, 5000);
     }
 });
 
@@ -28,29 +26,23 @@ chrome.storage.local.get('enabled', data => {
 chrome.storage.local.get('blockTw', data => {
     blocking = data.blockTw;
     if (blocking) {
-        console.log("Blocking Twitch stream!");
 
-        function getTwitchPath(path) {
-            return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        }
-
-        function removeTwitchStream () {
-            let twitchStream = '/html/body / div[3]';
-            if (getTwitchPath(twitchStream) == null) {
-            } else {
-                // console.log(twitchStream);
-                getTwitchPath(twitchStream).innerHTML = ``;
+        function getStream() {
+            streamElement = document.querySelector(".sc-jbwHOj");
+            iframe = document.getElementById("rufous-sandbox");
+            if(streamElement !== null) {
+                streamElement.style.display = "none";
+                iframe.style.display = "none";
                 successRm = true;
             }
         }
 
         if(!successRm) {
             setInterval(function() {
-                removeTwitchStream();
-            }, 3000);
+                // removeTwitchStream();
+                getStream();
+            }, 1000);
         }
 
-     } else {
-        console.log("Not blocking twitch stream...");
     }
 });
